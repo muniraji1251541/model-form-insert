8b71fe995fa19b898ec650c2c677217e1a506fa9
muniraji1251541 <muniraji1251541@gmail.com>
2023-02-03 11:55:09 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -9,7 +9,11 @@ class Model_Webpage(forms.ModelForm):
     class Meta:
         model=Webpage
         fields='__all__'
-
+        #fields=['topic_name','name']
+        #exclude=['name']
+        #labels={'topic_name':'tn','url':'ur'}
+        #widgets={'name':forms.PasswordInput,'url':forms.Textarea}
+        #help_texts={'name':'should not be integer','url':'provide proper url'}
 class Model_Access(forms.ModelForm):
     class Meta:
         model=Accessrecord
